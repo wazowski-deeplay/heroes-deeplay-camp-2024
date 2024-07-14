44461e76735761c97cd500a04d77474c165ccdc7
Денис Cтоляров <denis2002.stolyarov@mail.ru>
2024-07-14 17:26:05 +0700

    feat: INT-25 Add game entities

---
+++ game/src/main/java/io/deeplay/entities/Knight.java
@@ -0,0 +1,15 @@
+package io.deeplay.entities;
+
+public class Knight extends Unit {
+  public Knight() {
+    setMaxHP(10);
+    setNowHP(10);
+    setDamage(5);
+    setAccuracy(5);
+    setArmor(12);
+    setGeneral(false);
+  }
+
+  @Override
+  public void playMove(Unit targetUnit) {}
+}
---
+++ game/src/test/java/UnitTest.java
@@ -0,0 +1,70 @@
+import io.deeplay.entities.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class UnitTest {
+
+  @Test
+  void getMaxHPArcher() {
+    Archer unit = new Archer();
+    int expected = unit.getMaxHP();
+    int actual = 10;
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  void getMaxHPHealer() {
+    Healer unit = new Healer();
+    int expected = unit.getMaxHP();
+    int actual = 10;
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  void getNowHPKnight() {
+    Knight unit = new Knight();
+    int expected = unit.getNowHP();
+    int actual = 10;
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  void getDamageMage() {
+    Mage unit = new Mage();
+    int expected = unit.getDamage();
+    int actual = 5;
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  void getAccuracyArcher() {
+    Archer unit = new Archer();
+    int expected = unit.getAccuracy();
+    int actual = 6;
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  void getArmorHealer() {
+    Healer unit = new Healer();
+    int expected = unit.getArmor();
+    int actual = 12;
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  void isAlive() {
+    Healer unit = new Healer();
+    boolean expected = unit.isAlive();
+    boolean actual = true;
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  void isGeneral() {
+    Healer unit = new Healer();
+    boolean expected = unit.isGeneral();
+    boolean actual = false;
+    Assertions.assertEquals(expected, actual);
+  }
+}
