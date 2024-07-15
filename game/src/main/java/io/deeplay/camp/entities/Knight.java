c6d330d22f9234b20314092547aa303c55f25fdd
matthew8913 <matvey.yurkevich@gmail.com>
2024-07-15 19:32:23 +0700

    refactor: refactor codestyle

--- game/src/main/java/io/deeplay/camp/entities/Knight.java
+++ game/src/main/java/io/deeplay/camp/entities/Knight.java
@@ -2,8 +2,8 @@ package io.deeplay.camp.entities;
 
 public class Knight extends Unit {
   public Knight() {
-    setMaxHP(10);
-    setNowHP(10);
+    setMaxHp(10);
+    setNowHp(10);
     setDamage(5);
     setAccuracy(5);
     setArmor(12);
