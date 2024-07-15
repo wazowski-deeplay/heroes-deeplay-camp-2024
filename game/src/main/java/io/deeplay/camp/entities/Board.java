9216a416b28cd3c0164318f7eb30e33e15a7e8d6
matthew8913 <matvey.yurkevich@gmail.com>
2024-07-15 19:15:21 +0700

    feat: add board

---
+++ game/src/main/java/io/deeplay/camp/entities/Board.java
@@ -0,0 +1,33 @@
+package io.deeplay.camp.entities;
+
+import lombok.AllArgsConstructor;
+
+
+@AllArgsConstructor
+public class Board {
+    public static final int ROWS = 6;
+    public static final int COLUMNS = 3;
+
+    private Unit[][] units;
+
+    public void setUnit(int x, int y, Unit unit){
+        units[x][y] = unit;
+
+    }
+
+    public Unit getUnit(int x, int y){
+        return units[x][y];
+    }
+
+    public boolean isFull(){
+        for(Unit[] row : units){
+            for(Unit unit : row){
+                if(unit == null){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
