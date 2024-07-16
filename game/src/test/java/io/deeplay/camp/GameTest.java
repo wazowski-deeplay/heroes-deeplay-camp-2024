ed3eb4d58e150f38539ae36800dc1c43c22d44ee
matthew8913 <matvey.yurkevich@gmail.com>
2024-07-16 18:14:31 +0700

    fix: fix lombok

---
+++ game/src/test/java/io/deeplay/camp/GameTest.java
@@ -0,0 +1,39 @@
+package io.deeplay.camp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.deeplay.camp.events.ChangePlayerEvent;
+import io.deeplay.camp.mechanics.PlayerType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+  private Game game;
+
+  @BeforeEach
+  public void setUp() {
+    game = new Game();
+  }
+
+  @Test
+  void testChangePlayerRequestFromNotCurrentPlayer() {
+    game.gameState.setCurrentPlayer(PlayerType.FIRST_PLAYER);
+    ChangePlayerEvent changePlayerEvent = new ChangePlayerEvent(PlayerType.SECOND_PLAYER);
+
+    game.changePlayer(changePlayerEvent);
+
+    //Если запрос сделал второй игрок, текущий не должен поменяться
+    assertEquals(game.gameState.getCurrentPlayer(), PlayerType.FIRST_PLAYER);
+  }
+
+  @Test
+  void testChangePlayerFromCurrentPlayer() {
+    game.gameState.setCurrentPlayer(PlayerType.FIRST_PLAYER);
+    ChangePlayerEvent changePlayerEvent = new ChangePlayerEvent(PlayerType.FIRST_PLAYER);
+
+    game.changePlayer(changePlayerEvent);
+
+    //Если запрос сделал первый игрок, текущий не должен поменяться
+    assertEquals(game.gameState.getCurrentPlayer(), PlayerType.SECOND_PLAYER);
+  }
+}
