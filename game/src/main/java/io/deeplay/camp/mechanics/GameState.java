bcc72f5409571c5ea36e7bf139fc4f578b7ae8a9
Денис Cтоляров <denis2002.stolyarov@mail.ru>
2024-07-16 23:22:57 +0700

    feat: INT-53 Add playMove realization

--- game/src/main/java/io/deeplay/camp/mechanics/GameState.java
+++ game/src/main/java/io/deeplay/camp/mechanics/GameState.java
@@ -1,6 +1,7 @@
 package io.deeplay.camp.mechanics;
 
 import io.deeplay.camp.entities.Board;
+import io.deeplay.camp.entities.UnitType;
 import io.deeplay.camp.events.MakeMoveEvent;
 import io.deeplay.camp.events.PlaceUnitEvent;
 import lombok.Getter;
@@ -32,6 +33,7 @@ public class GameState {
 
   // методы чисто для применения, проверка происходит до их использования
   public void makeMove(MakeMoveEvent move) {
+      board.getUnit(move.getFrom().x(), move.getFrom().y()).playMove(board.getUnit(move.getTo().x(), move.getTo().y()));
     // что-то вроде достать атакующего и атакуемого юнита и сделать attacker.playMove(attacked)
   }
 
