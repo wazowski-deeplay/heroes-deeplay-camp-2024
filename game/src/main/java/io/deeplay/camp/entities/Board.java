9ba604dc8c36a5930c248af87039b265e40630a8
Денис Cтоляров <denis2002.stolyarov@mail.ru>
2024-07-17 12:38:22 +0700

    feat: INT-53 Add Mage-attack and optimization IsValidMove

--- game/src/main/java/io/deeplay/camp/entities/Board.java
+++ game/src/main/java/io/deeplay/camp/entities/Board.java
@@ -40,4 +40,8 @@ public class Board {
     }
     return count;
   }
+
+  public Unit[][] getUnits(){
+    return units;
+  }
 }
--- game/src/main/java/io/deeplay/camp/mechanics/GameState.java
+++ game/src/main/java/io/deeplay/camp/mechanics/GameState.java
@@ -1,6 +1,7 @@
 package io.deeplay.camp.mechanics;
 
 import io.deeplay.camp.entities.Board;
+import io.deeplay.camp.entities.Unit;
 import io.deeplay.camp.entities.UnitType;
 import io.deeplay.camp.events.MakeMoveEvent;
 import io.deeplay.camp.events.PlaceUnitEvent;
@@ -29,13 +30,24 @@ public class GameState {
 
   // методы чисто для применения, проверка происходит до их использования
   public void makeMove(MakeMoveEvent move) {
-      board.getUnit(move.getFrom().x(), move.getFrom().y()).playMove(board.getUnit(move.getTo().x(), move.getTo().y()));
-    // что-то вроде достать атакующего и атакуемого юнита и сделать attacker.playMove(attacked)
+      //Проверка на то, не является ли атакующий Магом
+      if(move.getAttacker().getUnitType() != UnitType.MAGE){
+        board.getUnit(move.getFrom().x(), move.getFrom().y()).playMove(board.getUnit(move.getTo().x(), move.getTo().y()));
+      }//Если является, то перебираем всех вражеских юнитов и наносим им удар
+      else{
+        for (int i = 0; i < board.getUnits().length; i++) {
+          for (int j = 0; j < board.getUnits()[i].length; j++) {
+            if(board.getUnits()[i][j].getPlayerType() != move.getAttacker().getPlayerType()){
+              board.getUnit(move.getFrom().x(), move.getFrom().y()).playMove(board.getUnit(i,j));
+            }
+          }
+        }
+      }
   }
 
   public void makePlacement(PlaceUnitEvent placeUnit) {}
 
-  public Board getBoard() {
+  public Board getCurrentBoard() {
     return board;
   }
 }
