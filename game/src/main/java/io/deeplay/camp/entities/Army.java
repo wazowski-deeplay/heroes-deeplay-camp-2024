af0472e9d025ab27aa2ee0a7f125def3777d6c12
Денис Cтоляров <denis2002.stolyarov@mail.ru>
2024-07-21 23:47:53 +0700

    feat: INT-53 Change makeMove and add Army entity

---
+++ game/src/main/java/io/deeplay/camp/entities/Army.java
@@ -0,0 +1,36 @@
+package io.deeplay.camp.entities;
+
+import io.deeplay.camp.mechanics.PlayerType;
+import lombok.Getter;
+
+@Getter
+public class Army {
+  Unit[] units;
+  public PlayerType owner;
+  public UnitType generalType;
+  public boolean isAliveGeneral = false;
+  public boolean isBuffed = false;
+
+  public Army(PlayerType owner) {
+    this.owner = owner;
+    units = new Unit[6];
+  }
+
+  public void fillArmy(Board board) {
+    int index = 0;
+    if (this.owner == PlayerType.FIRST_PLAYER) {
+      for (int i = 0; i < board.getUnits().length; i++) {
+        for (int j = 0; j < board.getUnits()[i].length / 2; j++) {
+          units[index++] = board.getUnit(i, j);
+        }
+      }
+    }
+    if (this.owner == PlayerType.SECOND_PLAYER) {
+      for (int i = 0; i < board.getUnits().length; i++) {
+        for (int j = board.getUnits().length / 2; j < board.getUnits()[i].length; j++) {
+          units[index++] = board.getUnit(i, j);
+        }
+      }
+    }
+  }
+}
