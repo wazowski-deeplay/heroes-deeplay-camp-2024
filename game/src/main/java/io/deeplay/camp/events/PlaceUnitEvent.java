fb9a1622ee07aa1d4ab42699cc42c66d3ad02b52
Maksim Ruf <max.progg@yandex.ru>
2024-07-22 22:56:54 +0700

    featch/INT-55-77-78-79 Redesing placement unit and add exeptions and logs - Для понимания что игрок хочет чтобы его растоновку проверили, он считает что она правильная была добавленна переменная inProcess - А так же иницилизация юнита как генерала в случаее если это указывает пользователь

--- game/src/main/java/io/deeplay/camp/events/PlaceUnitEvent.java
+++ game/src/main/java/io/deeplay/camp/events/PlaceUnitEvent.java
@@ -10,16 +10,20 @@ import lombok.Setter;
 @Setter
 public class PlaceUnitEvent extends Event {
 
-  private int colums;
+  private int columns;
   private int rows;
   private Unit unit;
+  private boolean inProcess;
 
   // Мы должны знать куда ставят, какого юнита, и кто ставит
   // playerType мы получаем из gameState, пример в тесте
-  public PlaceUnitEvent(int x, int y, @NonNull Unit unit, PlayerType playerType) {
-    this.colums = x;
+  public PlaceUnitEvent(
+      int x, int y, @NonNull Unit unit, PlayerType playerType, boolean inProcess, boolean general) {
+    this.columns = x;
     this.rows = y;
     this.unit = unit;
     unit.setPlayerType(playerType);
+    unit.setGeneral(general);
+    this.inProcess = inProcess;
   }
 }
