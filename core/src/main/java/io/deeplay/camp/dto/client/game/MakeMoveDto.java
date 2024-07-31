f673dbd23ec982842deccaa535b52d5a07bac123
Денис Cтоляров <denis2002.stolyarov@mail.ru>
2024-07-31 13:29:15 +0700

    feat: add-dto-constructor

--- core/src/main/java/io/deeplay/camp/dto/client/game/MakeMoveDto.java
+++ core/src/main/java/io/deeplay/camp/dto/client/game/MakeMoveDto.java
@@ -3,6 +3,8 @@ package io.deeplay.camp.dto.client.game;
 import io.deeplay.camp.dto.client.ClientDto;
 import io.deeplay.camp.dto.client.ClientDtoType;
 import java.util.UUID;
+
+import io.deeplay.camp.entities.Unit;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,9 +12,19 @@ import lombok.NoArgsConstructor;
 @Getter
 public class MakeMoveDto extends ClientDto {
   private UUID gamePartyId;
+  private int fromX;
+  private int fromY;
+  private int toX;
+  private int toY;
+  private Unit attacker;
 
-  public MakeMoveDto(UUID gamePartyId) {
+  public MakeMoveDto(UUID gamePartyId, int fromX, int fromY, int toX, int toY, Unit attacker) {
     super(ClientDtoType.MAKE_MOVE);
     this.gamePartyId = gamePartyId;
+    this.fromX = fromX;
+    this.fromY = fromY;
+    this.toX = toX;
+    this.toY = toY;
+    this.attacker = attacker;
   }
 }
--- core/src/main/java/io/deeplay/camp/dto/client/game/PlaceUnitDto.java
+++ core/src/main/java/io/deeplay/camp/dto/client/game/PlaceUnitDto.java
@@ -2,17 +2,35 @@ package io.deeplay.camp.dto.client.game;
 
 import io.deeplay.camp.dto.client.ClientDto;
 import io.deeplay.camp.dto.client.ClientDtoType;
+import io.deeplay.camp.entities.UnitType;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @NoArgsConstructor
 @Getter
 public class PlaceUnitDto extends ClientDto {
   private UUID gamePartyId;
+  private int columns;
+  private int rows;
+  private UnitType unitType;
+  private boolean inProcess;
+  private boolean general;
 
-  public PlaceUnitDto(UUID gamePartyId) {
+  public PlaceUnitDto(
+      UUID gamePartyId,
+      int x,
+      int y,
+      @NonNull UnitType unitType,
+      boolean inProcess,
+      boolean general) {
     super(ClientDtoType.PLACE_UNIT);
     this.gamePartyId = gamePartyId;
+    this.columns = x;
+    this.rows = y;
+    this.unitType = unitType;
+    this.inProcess = inProcess;
+    this.general = general;
   }
 }
