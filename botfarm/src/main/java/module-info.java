1b14a97fe030877f8cd3ae150113667be36d9b98
Maksim Ruf <max.progg@yandex.ru>
2024-08-08 16:20:20 +0700

    feat: add module botfarm for bots

---
+++ botfarm/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module io.deeplay.camp.botfarm {
+
+  requires java.desktop;
+  requires io.deeplay.camp.game;
+  requires org.slf4j;
+  requires static lombok;
+}
--- game/src/main/java/module-info.java
+++ game/src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module io.deeplay.camp.game {
+  exports io.deeplay.camp.game;
   exports io.deeplay.camp.game.entities;
   exports io.deeplay.camp.game.exceptions;
   exports io.deeplay.camp.game.mechanics;
   exports io.deeplay.camp.game.events;
-  exports io.deeplay.camp.game;
 
   requires java.base;
   requires static lombok;
